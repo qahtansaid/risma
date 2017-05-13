f9ac13b4f3d8bcfc72c9e76d3068791b073d4355
qahtansaid <qahtansaidmartak@gmail.com>
2017-05-13 16:51:08 +0700

    Initial commit

---
+++ app/src/main/java/com/animalshop/maretta/risma/TopMenuActivity.java
@@ -0,0 +1,21 @@
+package com.animalshop.maretta.risma;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+
+public class TopMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_top_menu);
+
+    }
+
+    public void onClickPesan(View v) {
+        Intent i = new Intent(this, OrderActivity.class);
+        startActivity(i); // Pindah Intent saat memencet CardView yang sudah diberi onClick bernama onClickPesan
+    }
+}
